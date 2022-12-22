b7cd22d418f2b9aa1cec297614cb0986308cc395
felisparkz <felisparkz@gmail.com>
2022-12-21 18:52:55 -0800

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,6 @@
 class Square:
     """Class that defines a square with a private instance attribute size"""
 
-
     def __init__(self, size=0):
         if isinstance(size, int):
             if size >= 0:
